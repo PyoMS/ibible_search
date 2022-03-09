945d119c1c963dbd585bb2e2b887945bbe4187a2
unknown <ms147850@gmail.com>
2022-03-09 15:59:58 +0900

    add InitCommandLineRunner

--- src/main/java/com/pyo/home/controller/IbiblesGetController.java
+++ src/main/java/com/pyo/home/controller/IbiblesGetController.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,7 +27,7 @@ public class IbiblesGetController {
 	@Autowired
 	private IbiblesGetService ibiblesService;
 	
-	@GetMapping("/getBibles")
+	@PostMapping("/getBibles")
 	@Transactional
 	public String getBible() throws Exception{
 		try {
